c4969fe5e8363fc08b19a6d6aabf393c0a122bd8
sagarhackerrank-debug <sagarhackerrank@gmail.com>
2026-09-19 11:52:24 +0530

    Two Sum Solution

---
+++ TwoSum.java
@@ -0,0 +1,37 @@
+// array two sum
+
+ 
+import java.util.Scanner;
+public class TwoSum{
+public static void main(String args[]){
+
+Scanner in=new Scanner(System.in);
+System.out.println( "enter a size");
+int n=in.nextInt();
+int[] arr=new int[n];
+System.out.println("enter an elemets");
+
+
+for(int i=0;i<n;i++)
+arr[i]=in.nextInt();
+System.out.println("enter your target");
+
+int t=in.nextInt();
+
+
+
+for(int i=0;i<n;i++){
+for(int j=i+1;j<n;j++){
+if(arr[i]+arr[j]==t){
+
+
+System.out.print(arr[i]+" ");
+System.out.print(arr[j]+" ");
+
+}
+}
+}
+
+
+}
+}
